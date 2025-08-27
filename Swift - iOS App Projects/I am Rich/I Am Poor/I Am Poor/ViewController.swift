69bcbf1be86b17c43a293b81a9cf2c777f315fa8
reddy-nithin <82646464+reddy-nithin@users.noreply.github.com>
2025-08-27 16:28:27 -0500

    Add files via upload

---
+++ Swift - iOS App Projects/I am Rich/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Nithin Reddy on 3/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
